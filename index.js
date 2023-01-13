480b19ec80c6f3678aee61f96f98114011304d6b
arnoll-tech <arnolbermudez28@gmail.com>
2023-01-13 14:33:48 -0500

    flata hacer desaparecer la card1 al momento de precioanr el boton

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const btn_submit = document.querySelector(".btn-submit");
-const card_2 = document.querySelector("#card_2");
+const card_2 = document.querySelector(".card_2");
 
 btn_submit.addEventListener("click", toggleCard);
 
 function toggleCard() {
-    
+    card_2.classList.toggle("inactive");
 }
