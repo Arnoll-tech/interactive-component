396b94868be24735c0053909474db9871e7c98b4
arnoll-tech <arnolbermudez28@gmail.com>
2023-01-13 14:17:52 -0500

    se crea la funcion para el cmabio de cards

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const btn_submit = document.querySelector(".btn-submit");
+const card_2 = document.querySelector("#card_2");
+
+btn_submit.addEventListener("click", toggleCard);
+
+function toggleCard() {
+    
+}
